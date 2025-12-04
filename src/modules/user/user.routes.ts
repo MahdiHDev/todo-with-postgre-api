50ee3619cb9684101843b38883951a26b570b990
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-12-04 11:16:10 +0600

    role base authentication implemented

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,16 +1,17 @@
 import express from "express";
+import auth from "../../middleware/auth";
 import { userController } from "./user.controller";
 
 const router = express.Router();
 
 router.post("/", userController.createUser);
 
-router.get("/", userController.getUser);
+router.get("/", auth("admin"), userController.getUser);
 
 router.get("/:id", userController.getSingleUser);
 
 router.put("/:id", userController.updateUser);
 
-router.delete("/:id", userController.deleteUser);
+router.delete("/:id", auth("admin", "user"), userController.deleteUser);
 
 export const userRoute = router;
